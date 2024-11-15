4021a26f2f7c5f5fe4bb06e5461dbe504969e90a
Santiago Florian <floranwsfrench@gmail.com>
2024-11-15 13:23:39 -0500

    Se ajustaron y dejaron los estilos en un archivo separado del componente

--- pensum/src/components/react/BarraMateriasReact.jsx
+++ pensum/src/components/react/BarraMateriasReact.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './BarraMaterias.css'
 
 // Definición del componente BarraMateriasReact
 const BarraMateriasReact = () => {
@@ -53,52 +54,6 @@ const BarraMateriasReact = () => {
           {nombre} {/* Texto del botón que corresponde al nombre de la categoría */}
         </button>
       ))}
-
-      {/* Estilos CSS para el diseño y los efectos visuales de los botones y la barra */}
-      <style>{`
-        .barra-categorias {
-          display: flex;
-          gap: 10px;
-          padding: 10px;
-          border-radius: 8px;
-          justify-content: center;
-          margin-bottom: 20px;
-          flex-wrap: wrap;
-        }
-
-        .categoria {
-          padding: 5px 10px;
-          color: black;
-          font-weight: bold;
-          border: 2px solid black;
-          border-radius: 5px;
-          text-align: center;
-          cursor: pointer;
-          transition: all 0.3s ease;
-        }
-
-        .categoria:hover {
-          transform: scale(1.05); // Aumenta el tamaño del botón al pasar el cursor
-        }
-
-        .categoria.activo {
-          box-shadow: 0 0 10px rgba(0,0,0,0.3); // Sombra para indicar el filtro activo
-          transform: scale(1.1); // Aumenta el tamaño del botón cuando está activo
-        }
-
-        /* Colores de fondo específicos para cada categoría */
-        .CO { background-color: #00b0f0; }
-        .CB { background-color: #ffff00; }
-        .BI { background-color: #93d150; }
-        .AI { background-color: #bfbfbf; }
-        .EX { background-color: #fcd5b4; }
-
-        @media screen and (max-width: 840px) {
-          .categoria{
-              width: 100%;
-          }
-      }
-      `}</style>
     </div>
   );
 };
