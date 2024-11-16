069161e372e038cead30984b2f7964ba4a207a20
Santiago Florian <floranwsfrench@gmail.com>
2024-11-16 11:06:38 -0500

    Se separa el uso de información de las materias con electivas en el servicio

--- pensum/src/services/materiasElectivas.ts
+++ pensum/src/services/materiasElectivas.ts
@@ -1,5 +1,10 @@
 import infoElectivas from "@/data/infoElectivas";
+import type { MateriaType } from "@/types/informacion";
 
 export function obtenerMateria(codigo: string){
     return infoElectivas.find(materia => materia.codigo === codigo)
+}
+
+export function obtenerMaterias(): MateriaType[]{
+    return infoElectivas
 }
