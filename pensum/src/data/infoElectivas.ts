3959027f231e2ff1d5b72ffc7d909030c35b220a
Santiago Florian <floranwsfrench@gmail.com>
2024-11-15 19:51:02 -0500

    Archivo con información de las electivas de carrera

---
+++ pensum/src/data/infoElectivas.ts
@@ -0,0 +1,415 @@
+import type { MateriaType } from '@/types/informacion'
+
+const infoElectivas: MateriaType[] = [
+  {
+    codigo: '36',
+    nombre: 'CURSO OBLIGATORIO I RF',
+    creditos: 3,
+    td: 2,
+    tc: 2,
+    ta: 5,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '37',
+    nombre: 'CURSO OBLIGATORIO II RF',
+    creditos: 3,
+    td: 2,
+    tc: 2,
+    ta: 5,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '38',
+    nombre: 'CURSO ELECTIVO I RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '41',
+    nombre: 'CURSO OBLIGATORIO III RF',
+    creditos: 3,
+    td: 2,
+    tc: 2,
+    ta: 5,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '42',
+    nombre: 'CURSO OBLIGATORIO IV RF',
+    creditos: 3,
+    td: 2,
+    tc: 2,
+    ta: 5,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '43',
+    nombre: 'CURSO ELECTIVO II RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '46',
+    nombre: 'CURSO ELECTIVO FLEXIBLE I RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '47',
+    nombre: 'CURSO ELECTIVO FLEXIBLE II RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '48',
+    nombre: 'CURSO ELECTIVO III RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '51',
+    nombre: 'CURSO ELECTIVO FLEXIBLE III RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  // {nombre: "", tipo: "", urlSyllabus: ""},
+  {
+    codigo: '52',
+    nombre: 'CURSO ELECTIVO FLEXIBLE IV RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+  {
+    codigo: '53',
+    nombre: 'CURSO ELECTIVO IV RF',
+    creditos: 4,
+    td: 4,
+    tc: 2,
+    ta: 6,
+    electivas: [
+      {
+        nombre: 'CURSO 1',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 2',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 3',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+      {
+        nombre: 'CURSO 4',
+        urlSyllabus:
+          'http://accioneduca.org/admin/archivos/modulos/ayudanos/prueba.pdf',
+        tipo: 'OPCION',
+      },
+    ],
+  },
+]
+
+export default infoElectivas
