08ee4d02f23f5e67db318941d33da96424f47422
Santiago Florian <floranwsfrench@gmail.com>
2024-12-15 21:40:57 -0500

    Actualización de tipos y documentación

--- pensum/src/types/informacion.ts
+++ pensum/src/types/informacion.ts
@@ -3,21 +3,32 @@ export interface NivelType {
     materias: MateriaType[]; // Lista de materias en el nivel
 }
 
+// CO: Complementario
+// CB: Ciencias Básicas
+// BI: Básicas de Ingeniería 
+// EX: Extrínsecas
+// AI: Aplicadas de Ingeniería
+//     EL: Electiva
+//     OPCION: Opcion Obligatoria
+// NA: No Aplica (Para espacios vacíos)
+
 export interface MateriaType {
+    id?: number;            //Identificador único de la materia
     codigo?: string;        // Código de la Materia
     nombre: string;        // Nombre Completo
     creditos?: number;      // Número de Créditos
-    info?: string;          // Información del espacio
     td?: number;            // Horas Trabajo Directo
     tc?: number;            // Horas Trabajo Colaborativo
     ta?: number;            // Horas Trabajo Autónomo
     urlSyllabus?: string;   // URL del syllabus o plan de la materia
-    tipo?: string;          // Tipo de Materia (CO, CB, BI, AI, EX)
-    electivas?: ElectivaType[]; //Lista de elecciones en el espacio
+    tipo?: string;          // Tipo de Materia (CO, CB, BI, AI, EX, EL, OPCION, NA)
+    electiva?: boolean;     // Establecer si es una materia electiva o no (De carrera)
+    opciones?: OpcionType[]; //Lista de elecciones en el espacio
 }
 
-export interface ElectivaType {
+export interface OpcionType {
     nombre: string;        // Nombre de la Electiva
     urlSyllabus: string;   // URL del syllabus o plan de la electiva
     tipo?: string;          // Tipo de Materia "OPCION"
+    ruta?: string;          // Ruta de aprendizaje de la opcion
 }
